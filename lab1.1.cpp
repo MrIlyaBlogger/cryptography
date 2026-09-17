c3886d08fa0817868839f70ce927a39de0b9ca5f
mr_ilyablogger <ilya5111082467@gmail.com>
2026-09-18 00:43:56 +0300

    function template

---
+++ lab1.1.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <cstdint>
+
+std::uint8_t value = {0b10110010};
+
+std::vector<int> p_block = {2, 0, 7, 4, 6, 1, 5, 3};
+
+std::uint8_t permute_bits (
+    std::uint8_t *value,
+    const std::vector<int> &p_block,
+    bool least_significant_first,
+    bool zero_is_first
+) {
+
+}
+
+int main() {
+
+}
